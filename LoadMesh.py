b53a18250fc88c80bdcfd72a0b39cc14b50884a5
JMprates27 <juanmarcelo27@gmail.com>
2024-11-24 12:36:21 -0300

    Adicionei comandos de interação e iluminação

--- LoadMesh.py
+++ LoadMesh.py
@@ -1,19 +1,53 @@
 from model import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from numpy import cross
+from numpy.linalg import norm
+
+# Função para calcular normais de uma face
+def calculate_normal(v1, v2, v3):
+    u = [v2[i] - v1[i] for i in range(3)]
+    v = [v3[i] - v1[i] for i in range(3)]
+    normal = cross(u, v)
+    magnitude = norm(normal)
+    if magnitude == 0:
+        return [0, 0, 0]
+    return [normal[i] / magnitude for i in range(3)]
 
 def ChairMesh():
+    material_diffuse = [0.3, 0.3, 0.3, 1.0]  # Cor difusa (cinza escuro)
+    material_ambient = [0.4, 0.4, 0.4, 1.0]  # Cor ambiente mais forte
+    material_specular = [1.0, 1.0, 1.0, 1.0]  # Reflexo especular
+    material_shininess = 32.0  # Brilho moderado
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_ambient)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_specular)
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_shininess)
+
+    # Desenhar as faces com iluminação
     glBegin(GL_QUADS)
     for face in chair_faces_vector4:
+        # Calcular a normal da face
+        v1 = chair_verticies_vector3[face[0]]
+        v2 = chair_verticies_vector3[face[1]]
+        v3 = chair_verticies_vector3[face[2]]
+        normal = calculate_normal(v1, v2, v3)
+
+        glNormal3fv(normal)  # Define a normal para a face
+
         x = 0
         for vertex in face:
-            x+=1
-            glColor3fv(colors[x])
+            x += 1
+            glColor3fv(colors[x])  # Define a cor do vértice
             glVertex3fv(chair_verticies_vector3[vertex])
     glEnd()
 
-    glBegin(GL_LINES)
-    for edge in chair_edges_vector2:
-        for vertex in edge:
-            glVertex3fv(chair_verticies_vector3[vertex])
-    glEnd()
+
+#comentei as linhas para tentar deixar o modelo mais bonito
+    # Desenhar as arestas (wireframe)
+    #glBegin(GL_LINES)
+    #for edge in chair_edges_vector2:
+        #for vertex in edge:
+            #glVertex3fv(chair_verticies_vector3[vertex])
+    #glEnd()
--- main.py
+++ main.py
@@ -24,8 +24,24 @@ def initialise():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     gluPerspective(60, (screen[0]/screen[1]), 0.1, 100.0)
-    glTranslate(0, 5, -12)
+    glTranslate(0, -1, -12)
 
+    # Ativar iluminação
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)  # Habilitar uma fonte de luz
+    glEnable(GL_DEPTH_TEST)  # Ativar teste de profundidade
+    #glEnable(GL_NORMALIZE)
+
+    # Configuração da luz
+    light_position = [5.0, 0.0, 10.0, 1.0]
+    light_diffuse = [1.0, 1.0, 1.0, 1.0]
+    light_ambient = [0.3, 0.3, 0.3, 1.0]
+    light_specular = [1.0, 1.0, 1.0, 1.0]
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse)
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular)
 
 
 def display():
@@ -52,6 +68,18 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    #glRotatef(2, 3, -10, -0)
+
+    keys = pg.key.get_pressed()
+    if keys[K_a]:
+        glRotatef(-2, 0, 2, 0)
+    if keys[K_d]:
+        glRotatef(2, 0, 2, 0)
+    if keys[K_w]:
+        glRotatef(-2, 2, 0, 0)
+    if keys[K_s]:
+        glRotatef(2, 2, 0, 0)
+
     display()
     ChairMesh()
     pg.display.flip()
